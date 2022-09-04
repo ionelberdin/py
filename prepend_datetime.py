e9b1eccfc73bd28f36b61d92a83a9691c1fe8d27
Ion <ionelberdin@gmail.com>
2022-09-04 20:19:10 +0200

    how to use + default folderpath

--- prepend_datetime.py
+++ prepend_datetime.py
@@ -1,6 +1,10 @@
 '''
 # Idea behind this script
-Rename files to include their datetime info at the begining of the file name.
+Some media files such as pictures or videos capture their original date in
+metadata within the file. 
+In order to organise these files by that datetime, which may differ from the
+file creation datetime, the idea is to automatically rename them including 
+their datetime info at the begining of the file name.
 
 # Limitations
 * It doesn't check if a file with the new name already exists, so it could
@@ -10,6 +14,15 @@ Rename files to include their datetime info at the begining of the file name.
 * It only renames images with EXIF data. It could be extended for video files.
 * It doesn't produce any log file with the summary of the operations performed.
 * It doesn't explore paths recursively.
+
+# How to use this script
+1. From the terminal:
+>>> python prepend_datetime.py -f path_to_folder_with_files_to_rename
+
+2. From other Python script:
+>>> from prepend_datetime import prepend_datetime_to_filname
+>>> # do your thing to retrive the path to the files to rename
+>>> # then use the preprend_datetime_to_filename function for each of them
 '''
 
 import exifread  # to extract metadata from picture files
@@ -59,6 +72,10 @@ def get_original_datetime(filepath):
 if __name__ == '__main__':
     from argparse import ArgumentParser
     parser = ArgumentParser()
-    parser.add_argument('folderpath')
+    arg_help = "Relative or absolute path to folder with files to rename"
+    parser.add_argument('-f', '--folderpath', help=arg_help, 
+                        default=os.path.dirname(__file__))
     arguments = parser.parse_args()
-    apply_function_to_files_in_folder(arguments.folderpath, prepend_datetime_to_filename)
+    folderpath = arguments.folderpath
+
+    apply_function_to_files_in_folder(folderpath, prepend_datetime_to_filename)
